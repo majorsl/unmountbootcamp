bdb42ef94b16d8e8ffb4999fb1f656f2ebb37d56
majorsl <steve@themajorshome.com>
2016-10-02 18:44:51 -0400

    Unmounts BOOTCAMP from Desktop in macOS.
    This is useful in a dual boot environment where your users might be
    confused or try to use a mounted Boot Camp partition while in the
    macOS. You could use it as part of a launchd job after a user logs in.

---
+++ unmountbootcamp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#version 1.0
+#Unmounts BOOTCAMP from Desktop in macOS.
+
+#Get the disk
+BOOTCAMP=`diskutil list | grep BOOTCAMP | awk '{ print substr($0,69,9) }'`
+
+if [ $BOOTCAMP = "" ]; then
+	exit 0
+fi
+
+diskutil unmount /dev/"$BOOTCAMP"
+
+exit 0
